091cac9277d8523f23c9b3255e519268efe4b648
fmi <fmi@fmi-547-1.fmi.uni-plovdiv.bg>
2022-11-13 09:13:14 +0200

    initial

--- WeatherSocialApp/src/uni/fmi/masters/HelloServlet.java
+++ WeatherSocialApp/src/uni/fmi/masters/HelloServlet.java
@@ -1,6 +1,13 @@
 package uni.fmi.masters;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -19,16 +26,31 @@ public class HelloServlet extends HttpServlet {
 	UserBean user = null;
 	
 	ArrayList<StatusBean> statuses = new ArrayList<>();
+	ArrayList<FriendsRequestsBean> requests = 
+			new ArrayList<FriendsRequestsBean>();
 	
     /**
      * @see HttpServlet#HttpServlet()
      */
     public HelloServlet() {
         
+  
+    }
+    
+    private void startingData() {
         this.statuses.add(new StatusBean(30, "Варна", "Време за море!"));
         statuses.add(new StatusBean(-68, "Аляска", "Малко захладня!"));
         statuses.add(new StatusBean(60, "Пловдив", "Айляк майна!"));
         
+        UserBean friend1 = 
+        		new UserBean("goshko", "hubaveca@abv.bg");
+        UserBean friend2 = 
+        		new UserBean("mariika", "sramejlivata@abv.bg");
+        
+        requests.add(new FriendsRequestsBean(friend1, user, "friends", true));
+        
+        requests.add(new FriendsRequestsBean(friend2, user, "friends", true));
+              
     }
 
 	/**
@@ -42,13 +64,22 @@ public class HelloServlet extends HttpServlet {
 		switch(action) {
 		case "login":
 			
-			login(request, response);
+			try {
+				login(request, response);
+			} catch (ServletException | IOException | SQLException e1) {
+				e1.printStackTrace();
+			}
 			
 			break;
 			
 		case "register":
 			
-			register(request, response);
+			try {
+				register(request, response);
+			} catch (ServletException | IOException | SQLException e) {
+				
+				e.printStackTrace();
+			}
 			
 			break;
 			
@@ -68,12 +99,24 @@ public class HelloServlet extends HttpServlet {
 			response.getWriter().append("completed");
 		
 			break;
+			
+		case "friends":
+			goFriends(request, response);			
+			
+			break;
 			default:
 				
 				
 		}
 		
 	
+	}
+
+	private void goFriends(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("userFriends", requests);
+		
+		request.getRequestDispatcher("friends.jsp")
+			.forward(request, response);
 	}
 
 	private void goHome(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -84,7 +127,7 @@ public class HelloServlet extends HttpServlet {
 	}
 
 	private void register(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws ServletException, IOException, SQLException {
 		String regUsername = request.getParameter("register-user");
 		String regPassword = request.getParameter("register-pass");
 		String repeatPassword = 
@@ -105,26 +148,67 @@ public class HelloServlet extends HttpServlet {
 		
 		user = new UserBean(regUsername, regPassword, email);
 		
-		request.getRequestDispatcher("index.html")
+		startingData();
+		
+		if(insertUserToDB(user)) {
+			request.getRequestDispatcher("index.html")
 			.forward(request, response);
+		}else {
+			
+			request.setAttribute("message", "Register not successfull");
+			
+			request.getRequestDispatcher("error.jsp")
+			.forward(request, response);
+		}
+		
+
 	}
 
-	private void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(user == null)
-			return;
+	private boolean insertUserToDB(UserBean user) throws SQLException {
+		Connection conn = null;
 		
-		String username = request.getParameter("usernamesdsd");
+		try {
+			conn = getConnection();
+			
+			String sql = "INSERT INTO USERS "
+					+ "(USERNAME, PASSWORD, EMAIL) "
+					+ " VALUES (?,?,?)";
+			
+			PreparedStatement pst = conn.prepareStatement(sql);
+			
+			pst.setString(1, user.getUsername());
+			pst.setString(2, hashPassword(user.getPassword()));
+			pst.setString(3, user.getEmail());
+
+			if(pst.executeUpdate() == 1)
+				return true;	
+			
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		}finally {
+			if(conn != null)
+				conn.close();
+		}
+		
+		return false;		
+		
+	}
+
+	private void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+			
+		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
-		if(username.equalsIgnoreCase(user.getUsername()) 
-				&& password.equals(user.getPassword())) {
+		if(validateUser(username,password)) {
 			
 			request.setAttribute("loggedUser", user);
 			request.getRequestDispatcher("profile.jsp")
 				.forward(request, response);				
 			
 		}else {
-			request.getRequestDispatcher("error.html")
+			request.setAttribute("message", "Wrong username/password");
+			request.getRequestDispatcher("error.jsp")
 				.forward(request, response);
 		}
 	}
@@ -141,5 +225,83 @@ public class HelloServlet extends HttpServlet {
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		super.doDelete(req, resp);
+	}
+	
+	
+	private boolean validateUser(String username, String password) throws SQLException {
+		
+		Connection conn = null;
+		ResultSet rs = null;
+		
+		password = hashPassword(password);
+		
+		try {
+			conn = getConnection();
+			String sql  = "SELECT ID, email, avatar "
+					+ "FROM USERS WHERE USERNAME=? AND PASSWORD=?";
+			
+			PreparedStatement pst = conn.prepareStatement(sql);
+			pst.setString(1, username);
+			pst.setString(2, password);
+			
+			rs = pst.executeQuery();
+			
+			if(rs.first()) {
+				
+				user = new UserBean();
+				
+				user.setUsername(username);
+				user.setPassword(password);		
+				user.setEmail(rs.getString("email"));				
+				user.setId(rs.getInt("id"));
+				user.setAvatar(rs.getString("avatar"));				
+				
+				return true;
+			}	
+		}catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}finally {
+			if(rs != null)
+				rs.close();
+			
+			if(conn != null)
+				conn.close();
+		}
+		
+		return false;		
+	}
+	
+	private String hashPassword(String password) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(password.getBytes());
+			
+			byte[] bytes = md.digest();
+			
+			for(int i = 0; i < bytes.length; i++) {
+				sb.append((char)bytes[i]);
+			}
+			
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		return sb.toString();		
+	}
+	
+	private Connection getConnection() throws SQLException {
+		
+		try {
+			Class.forName("org.h2.Driver");
+			
+		}catch(ClassNotFoundException ex) {
+			System.out.println(ex.getMessage());
+		}
+		
+		return DriverManager.getConnection("jdbc:h2:~/test", "sa", "123");
 	}
 }
--- WeatherSocialApp/src/uni/fmi/masters/UserBean.java
+++ WeatherSocialApp/src/uni/fmi/masters/UserBean.java
@@ -3,16 +3,35 @@ package uni.fmi.masters;
 import java.io.Serializable;
 
 public class UserBean implements Serializable{
+	
+//	CREATE TABLE USERS(
+//			ID INT PRIMARY KEY AUTO_INCREMENT, 
+//			USERNAME VARCHAR(50)  NOT NULL UNIQUE,
+//			PASSWORD VARCHAR(32) NOT NULL,
+//			EMAIL VARCHAR(250) NOT NULL UNIQUE,
+//			AVATAR VARCHAR(250) DEFAULT 'https://static.wikia.nocookie.net/familyguyfanon/images/0/02/Stewie_Griffin.png/revision/latest?cb=20161119043926');
+
 
 	private String username;
 	private String password;
 	private String email;
+	private int id;
+	private String avatar;
 			
 	public UserBean(String username, String password, String email) {
 		super();
 		this.username = username;
 		this.password = password;
 		this.email = email;
+	}
+
+	public UserBean(String username, String email) {
+		this.username = username;
+		this.email = email;
+	}
+
+	public UserBean() {
+		// TODO Auto-generated constructor stub
 	}
 
 	public String getUsername() {
@@ -38,6 +57,21 @@ public class UserBean implements Serializable{
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+		
 }
